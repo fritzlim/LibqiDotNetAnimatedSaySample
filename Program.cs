89e052d624b5ac41b1f8e77b1f6c3b62d67a69c1
malaybaku <njslyr.trpg+harriet@gmail.com>
2016-03-14 15:51:17 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+using Baku.LibqiDotNet;
+
+namespace TestAnimatedSay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //作業ディレクトリだけでなく"dlls"というフォルダのライブラリも実行中に参照できるよう設定を変更
+            PathModifier.AddEnvironmentPaths(Path.Combine(Environment.CurrentDirectory, "dlls"));
+
+            //HelloWorldの対象とするマシンのアドレスをIPとポート(ポートは通常9559)で指定
+            string address = "tcp://xxx.xxx.xxx.xxx:9559";
+            var session = QiSession.Create(address);
+
+
+            //AnimatedSay::sayに渡す典型的なannotated text
+            var text = new QiString(
+                "^start(animation/Stand/Gestures/Hey_1) " + 
+                "Hello, this is a typical sample sentence for animated say module, to check some autonomous motion suited for the conversation " +
+                "^wait(animation/Stand/Gestures/Hey_1)"
+                );
+
+            //AnimatedSay::sayの第二引数に渡す、モーションのモードを表す単一ペアの辞書
+            var config = QiMap<QiString, QiString>.Create(new[]
+            {
+                new KeyValuePair<QiString, QiString>(
+                    new QiString("bodyLanguageMode"),
+                    new QiString("contextual")
+                    )
+            });
+
+            var animatedSayArgs = QiTuple.Create(text, config);
+            Debug.WriteLine(animatedSayArgs.Signature);
+            Debug.WriteLine(animatedSayArgs.Dump());
+
+
+            var animatedSpeech = session.GetService("ALAnimatedSpeech");
+
+            //1. CallメソッドはQiMap型のシグネチャをハンドリングできない(.NETラッパー側の不備)ので
+            //     低レイヤ呼び出しのCallDirectを用いる
+
+            //2. "(sm)"はqi Frameworkの型を表す文字列で、
+            //   関数の引数に文字列("s")と、動的型("m")からなるタプル(全体を囲うカッコ)であることを示す
+            //   詳細はは公式の型-文字列対応表(http://doc.aldebaran.com/libqi/api/cpp/type/signature.html)などを参照
+
+            //3. CallDirectの戻り値は非同期形式(QiFuture)なのでGetValue()での結果取得により明示的にブロックして同期処理化
+            //   (非同期で使いたければGetValue()を後で呼ぶ)
+            var res = animatedSpeech
+                .CallDirect("say::(sm)", animatedSayArgs.QiValue)
+                .GetValue();
+
+            //Void型が返却されるのを確認
+            Debug.WriteLine(res.ContentValueKind);
+
+        }
+    }
+
+    static class PathModifier
+    {
+        public static void AddEnvironmentPaths(params string[] paths)
+        {
+            var path = new[] { Environment.GetEnvironmentVariable("PATH") ?? "" };
+
+            string newPath = string.Join(Path.PathSeparator.ToString(), path.Concat(paths));
+
+            Environment.SetEnvironmentVariable("PATH", newPath);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+# LibqiDotNetAnimatedSaySample
+
+
+Baku
+2016/Mar/14
+
+1. About / How to use
+3. License
+4. Contact
+
+
+## About / How to use
+本レポジトリはAldebaran社のロボットが利用している通信フレームワーク`libqi`の.NETラッパーである[`Baku.LibqiDotNet`](https://github.com/malaybaku/BakuLibQiDotNet)の利用例を紹介するサンプルです。
+
+具体的にはモーションつき発話を行う[ALAnimatedSpeech::say](http://doc.aldebaran.com/2-1/naoqi/audio/alanimatedspeech-api.html#ALAnimatedSpeechProxy::say__ssCR)を.NETから呼び出す例を紹介しています。
+
+とある方から`Baku.LibqiDotNet`(NuGetパッケージのver1.0.1)の動作不具合に関するご質問を頂いたため、回答というか回避策の紹介を目的に公開しました。
+
+使い方については`Baku.LibqiDotNet`の利用法を把握していれば`Program.cs`の内容で十分伝わるハズなので詳細は記載しません。
+
+
+## License
+`Baku.LibqiDotNet`のライセンス(MIT License)に従います。
+
+## Contact
+
+- [Blog](http://www.baku-dreameater.net/)
+- [Twitter](https://twitter.com/baku_dreameater)
+
